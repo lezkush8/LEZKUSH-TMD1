d63fad24b9bd988bc42f4830eaf0df64dd348009
Next5x_timnasa <botuse41@gmail.com>
2025-04-30 11:48:54 +0300

    Update proprio.js

--- commandes/proprio.js
+++ commandes/proprio.js
@@ -6,7 +6,7 @@ const {zokou} =require("../framework/zokou")
 
 
 
-zokou({nomCom:"redemarrer",categorie:"Mods",reaction:"👨🏿‍💼"},async(dest,z,com)=>{
+zokou({nomCom:"restat",categorie:"Mods",reaction:"👨🏿‍💼"},async(dest,z,com)=>{
 
 
   
@@ -14,12 +14,12 @@ const{repondre,ms,dev,superUser}=com;
 
   if(!superUser)
   {
-    return repondre("Cette commande est réservée au propriétaire du bot");
+    return repondre("bot is reatating.......");
   }
 
   const {exec}=require("child_process") ;
 
-    repondre("* Redémarrage du bot en cour ...*");
+    repondre("* restat bot enable wait to reduce...*");
 
   exec("pm2 restart all");
   
