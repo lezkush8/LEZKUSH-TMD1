ee6a374d0930134237a95815351f3c851e9e09fe
Next5x_timnasa <botuse41@gmail.com>
2025-05-01 09:30:24 +0300

    Create timnasa_repo.js

---
+++ commandes/timnasa_repo.js
@@ -0,0 +1,46 @@
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+
+zokou({ nomCom: "repo", catégorie:"General", reaction: "👊", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://github.com/lezkush8/lezkush-bot';
+  const img = 'https://files.catbox.moe/bsvq5j.jpg';
+
+  try {
+    const response = await fetch(githubRepo);
+    const data = await response.json();
+
+    if (data) {
+      const repoInfo = {
+        stars: data.stargazers_count,
+        forks: data.forks_count,
+        lastUpdate: data.updated_at,
+        owner: data.owner.login,
+      };
+
+      const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+      const gitdata = `*ʜᴇʟʟᴏᴡ ᴡʜᴀᴛsᴀᴀᴘ ᴜsᴇʀ
+ᴛʜɪs ɪs* *𝐋𝚵𝚭𝐊𝐔𝐒𝚮-𝚻𝚳𝐃 .*\n sᴜᴘᴘᴏʀᴛ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ *ʙʏ*,  https://whatsapp.com/channel/0029VajweHxKQuJP6qnjLM31
+
+╔═━━━━════──────➳
+║╔═━━━━━━════─━━─➳
+║║ 🗼 *REPOSITORY:* ${data.html_url}
+║║ 🌟 *STARS:* ${repoInfo.stars}
+║║ 🧧 *FORKS:* ${repoInfo.forks}
+║║ 📅 *RELEASE DATE:* ${releaseDate}
+║║ 🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
+║║ 👨‍💻 *OWNER:* *𝐋𝚵𝚭𝐊𝐔𝐒𝚮*
+║║ 💞 *NAME:* *LEZKUSH*
+║║ 🥰 *ENJOY TO USE 𝐋𝚵𝚭𝐊𝐔𝐒𝚮-𝚻𝚳𝐃 * ╚══━━━━════─━━━━──➳
+╚══━━━━════─━━━━──➳`;
+
+      await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+    } else {
+      console.log("Could not fetch data");
+    }
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+});
