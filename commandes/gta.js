2d689fd94789a289b808f1aa0596aa0ce2c9373d
Next5x_timnasa <botuse41@gmail.com>
2025-05-05 16:54:31 +0300

    Update gta.js

--- commandes/gta.js
+++ commandes/gta.js
@@ -24,7 +24,7 @@ zokou({
       'image': {
         'url': _0x93b128
       },
-      'caption': "*powered by RAHMANI MD*"
+      'caption': "*powered by LEZKUSH-TMD*"
     }, {
       'quoted': _0x4eb9fe
     });
