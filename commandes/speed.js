dc5acf0ddca0d30a0350c5e5693aec4f2b190f5b
Next5x_timnasa <botuse41@gmail.com>
2025-05-23 15:35:00 +0300

    Update speed.js

--- commandes/speed.js
+++ commandes/speed.js
@@ -1,154 +1,52 @@
 const {
   zokou
-} = require("./../framework/zokou");
-const {
-  format,
-  runtime
-} = require('../framework/mesfonctions');
-const os = require('os');
-const speed = require('performance-now');
-const {
-  performance
-} = require('perf_hooks');
-const conf = require('../set');
-
-zokou(
-  {
-    nomCom: 'ping',
-    categorie: 'General',
-    reaction: '🤔',
-    alias: ['p']
-  },
-
-  async (dest, zk, commandOptions) => {
-    const {
-      ms, arg, repondre
-    } = commandOptions;
-    const start = new Date().getTime();
-    const msg = await zk.sendMessage(dest, {
-      text: '*timnasa creator*',
-    }, {
-      quoted: ms
-    });
-    const end = new Date().getTime();
-    const ping = end - start;
-    await zk.sendMessage(dest, {
-      text: `*.𝐋𝚵𝚭𝐊𝐔𝐒𝚮-𝚻𝚳𝐃 ping is speed.999999999🤠*
- *${ping} ms*`, edit: {
-        id: msg.key.id, remoteJid: dest
-      }});
-    await zk.sendMessage(dest, {
-      react: {
-        text: "🌡️", key: ms.key
-      }})
-  }
-)
-
-zokou(
-  {
-    nomCom: 'desc',
-    reaction: 'ℹ',
-    alias: ['i']
-  },
-
-  async (dest, zk, commandOptions) => {
-    const {
-      ms, arg, repondre
-    } = commandOptions;
-    // data
-    const tumbUrl = 'https://i.ibb.co/wyYKzMY/68747470733a2f2f74656c656772612e70682f66696c652f6530376133643933336662346361643062333739312e6a7067.jpg';
-    const used = process.memoryUsage();
-    const cpus = os.cpus().map(cpu => {
-      cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0);
-      return cpu
-    });
-    const cpu = cpus.reduce((last, cpu, _, {
-      length
-    }) => {
-      last.total += cpu.total
-      last.speed += cpu.speed / length
-      last.times.user += cpu.times.user
-      last.times.nice += cpu.times.nice
-      last.times.sys += cpu.times.sys
-      last.times.idle += cpu.times.idle
-      last.times.irq += cpu.times.irq
-      return last
-    }, {
-      speed: 0,
-      total: 0,
-      times: {
-        user: 0,
-        nice: 0,
-        sys: 0,
-        idle: 0,
-        irq: 0
-      }
-    });
-    let timestamp = speed();
-    let latensi = speed() - timestamp;
-    let neww = performance.now();
-    let oldd = performance.now();
-    const response = `
-Response Speed ${latensi.toFixed(4)} _Second_ \n ${oldd - neww} _miliseconds_\n\nRuntime : ${runtime(process.uptime())}
-
-💻 Info Server
-    RAM: ${format(os.totalmem() - os.freemem())} / ${format(os.totalmem())}
-
-_NodeJS Memory Usaage_
-    ${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n')}
-
-${cpus[0] ? `_Total CPU Usage_
-    ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
-_CPU Core(s) Usage (${cpus.length} Core CPU)_
-    ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}`: ''}
-    `.trim();
-    await zk.sendMessage(dest, {
-      text: response,
-      contextInfo: {
-        externalAdReply: {
-          showAdAttribution: true,
-          title: `${conf.BOT}`,
-          body: `${latensi.toFixed(4)} Second`,
-          thumbnailUrl: `${tumbUrl}`,
-          sourceUrl: global.link,
-          mediaType: 1,
-          renderLargerAbhinail: true
+} = require(__dirname + "/../framework/zokou");
+const moment = require("moment-timezone");
+const set = require(__dirname + '/../set');
+moment.tz.setDefault('' + set.TZ);
+zokou({
+  'nomCom': 'ping',
+  'categorie': 'General'
+}, async (_0x707b9d, _0x307f4b, _0x4f3966) => {
+  let {
+    ms: _0x18154c
+  } = _0x4f3966;
+  const {
+    time: _0x674b36,
+    date: _0x389885
+  } = {
+    'time': moment().format('HH:mm:ss'),
+    'date': moment().format("DD/MM/YYYY")
+  };
+  const _0x448cd1 = Math.floor(Math.random() * 0x64) + 0x1;
+  try {
+    await _0x307f4b.sendMessage(_0x707b9d, {
+      'audio': {
+        'url': "https://files.catbox.moe/11f9pe.mp3"
+      },
+      'mimetype': "audio/mp4",
+      'ptt': true,
+      'contextInfo': {
+        'isForwarded': true,
+        'forwardedNewsletterMessageInfo': {
+          'newsletterJid': "120363332512801418@newsletter",
+          'newsletterName': "╭𝐋𝚵𝚭𝐊𝐔𝐒𝚮-𝚻𝚳𝐃",
+          'serverMessageId': 0x8f
+        },
+        'forwardingScore': 0x3e7,
+        'externalAdReply': {
+          'title': "𝐋𝚵𝚭𝐊𝐔𝐒𝚮-𝚻𝚳𝐃",
+          'body': "⭐ Pong: " + _0x448cd1 + "ms\n📅 *Date:* " + _0x389885 + "\n⏰ *Time:* " + _0x674b36,
+          'thumbnailUrl': "https://files.catbox.moe/bsvq5j.jpg",
+          'mediaType': 0x1,
+          'renderSmallThumbnail': true
         }
       }
     }, {
-      quoted: ms
-    })
+      'quoted': _0x18154c
+    });
+  } catch (_0x163c20) {
+    console.log("❌ Ping Command Error: " + _0x163c20);
+    repondre("❌ Error: " + _0x163c20);
   }
-);
-
-zokou(
-  {
-    nomCom: 'runtime',
-    reaction: '🚨',
-    alias: ['uptime']
-  },
-  async (dest, zk, commandOptions) => {
-    const {
-      ms
-    } = commandOptions;
-    const tumbUrl = 'https://i.ibb.co/wyYKzMY/68747470733a2f2f74656c656772612e70682f66696c652f6530376133643933336662346361643062333739312e6a7067.jpg';
-    const runtimetext = `🚨 *Bot Have Been Running For ${runtime(process.uptime())}* 🚨`;
-    zk.sendMessage(dest, {
-      text: runtimetext,
-      contextInfo: {
-        externalAdReply: {
-          showAdAttribution: true,
-          title: `${conf.BOT}`,
-          body: `「 RUNTIME 」`,
-          thumbnailUrl: tumbUrl,
-          sourceUrl: global.link,
-          mediaType: 1,
-          renderLargerAbhinail: true
-        }
-      }
-    }, {
-      quoted: ms
-    })
-  }
-);
-    
+})
