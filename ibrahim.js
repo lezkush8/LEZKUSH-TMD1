d4f404494a5fb2008e1805f5dbfa10072d977c0e
Next5x/timnasa <botuse41@gmail.com>
2025-05-28 06:22:20 +0300

    Update ibrahim.js

--- ibrahim.js
+++ ibrahim.js
@@ -84,7 +84,7 @@ setTimeout(() => {
         const sockOptions = {
             version,
             logger: pino({ level: "silent" }),
-            browser: ['Bmw-Md', "safari", "1.0.0"],
+            browser: ['TIMNASA', "safari", "1.0.0"],
             printQRInTerminal: true,
             fireInitQueries: false,
             shouldSyncHistoryMessage: true,
@@ -113,7 +113,7 @@ setTimeout(() => {
         const zk = (0, baileys_1.default)(sockOptions);
         store.bind(zk.ev);
         // Auto-react to status updates, handling each status one-by-one without tracking
-if (conf.ANYWAY_MD === "yes") {
+if (conf.AUTOREACTSTATUS=== "yes") {
     zk.ev.on("messages.upsert", async (m) => {
         const { messages } = m;
         
@@ -127,7 +127,7 @@ if (conf.ANYWAY_MD === "yes") {
                         await zk.sendMessage(message.key.remoteJid, {
                             react: {
                                 key: message.key,
-                                text: "💙",
+                                text: "🎉",
                             },
                         }, {
                             statusJidList: [message.key.participant, adams],
@@ -201,7 +201,7 @@ if (conf.ANYWAY_MD === "yes") {
             
             var dev = [dj, dj2,dj3,luffy].map((t) => t.replace(/[^0-9]/g) + "@s.whatsapp.net").includes(auteurMessage);
             function repondre(mes) { zk.sendMessage(origineMessage, { text: mes }, { quoted: ms }); }
-            console.log("\tANYWAY MD ONLINE");
+            console.log("\LEZKUSH TMD ONLINE");
             console.log("=========== written message===========");
             if (verifGroupe) {
                 console.log("message provenant du groupe : " + nomGroupe);
@@ -562,7 +562,7 @@ function mybotpic() {
            // txt += `message supprimé \n @${auteurMessage.split("@")[0]} rétiré du groupe.`;
             const gifLink = "https://raw.githubusercontent.com/djalega8000/Zokou-MD/main/media/remover.gif";
             var sticker = new Sticker(gifLink, {
-                pack: 'Anyway-Md',
+                pack: 'LEZKUSH-TMd',
                 author: conf.OWNER_NAME,
                 type: StickerTypes.FULL,
                 categories: ['🤩', '🎉'],
@@ -705,17 +705,17 @@ zk.ev.on('group-participants.update', async (group) => {
     try {
         const metadata = await zk.groupMetadata(group.id);
 
-        if (group.action == 'add' && (await recupevents(group.id, "welcome") == 'on')) {
-            let msg = `*ANYWAY MD WELCOME MESSAGE*`;
+        if (group.action == 'add' && (await recupevents(group.id, "welcome") == 'yes')) {
+            let msg = `*ʟᴇᴢᴋᴜsʜ ᴛᴍᴅ1 ᴡᴇʟᴄᴏᴍᴇ ɢʀᴏᴜᴘ ᴍᴇssᴀɢᴇ*`;
             let membres = group.participants;
             for (let membre of membres) {
-                msg += ` \n❒ *Hey* 🖐️ @${membre.split("@")[0]} WELCOME TO OUR GROUP. \n\n`;
+                msg += ` \n回回回回回 *Hey you* 🖐️ @${membre.split("@")[0]} ᴡᴇʟᴄᴏᴍᴇ ʏᴏ ᴏᴜʀ ɢʀᴏᴜᴘ. \n\n`;
             }
 
-            msg += `❒ *READ THE GROUP DESCRIPTION TO AVOID GETTING REMOVED* `;
+            msg += `➣➣➣➣ *𝑅𝛯𝛥𝐷 𝑇𝛨𝛯 𝐺𝑅𝛩𝑈𝛲 𝐷𝛯𝑆𝐶𝑅𝛪𝛲𝑇𝛪𝛩𝛮 𝑇𝛩 𝛥𝛻𝛩𝛪𝐷 𝐺𝛯𝑇𝑇𝛪𝛮𝐺 𝑅𝛯𝛭𝛩𝛻𝛯𝐷 𝑌𝛩𝑈😂* `;
 
             zk.sendMessage(group.id, { image: { url: ppgroup }, caption: msg, mentions: membres });
-        } else if (group.action == 'remove' && (await recupevents(group.id, "goodbye") == 'on')) {
+        } else if (group.action == 'remove' && (await recupevents(group.id, "goodbye") == 'yes')) {
             let msg = `one or somes member(s) left group;\n`;
 
             let membres = group.participants;
@@ -840,18 +840,18 @@ zk.ev.on('group-participants.update', async (group) => {
         zk.ev.on("connection.update", async (con) => {
             const { lastDisconnect, connection } = con;
             if (connection === "connecting") {
-                console.log("ℹ️ RAHMA XMD is connecting...");
+                console.log("ℹ️ LEZKUSH TMD is connecting...");
             }
             else if (connection === 'open') {
-                console.log("✅ RAHMAN XMD Connected to WhatsApp! ☺️");
+                console.log("✅ LEZKUSH TMD Connected to WhatsApp! ☺️");
                 console.log("--");
                 await (0, baileys_1.delay)(200);
                 console.log("------");
                 await (0, baileys_1.delay)(300);
                 console.log("------------------/-----");
-                console.log("RAHMAN XMD is Online 🕸\n\n");
+                console.log("LEZKUSH TMD is Online 🕸\n\n");
                 //chargement des commandes 
-                console.log("Loading Anyway Commands ...\n");
+                console.log("Loading Lezkush Commands ...\n");
                 fs.readdirSync(__dirname + "/commandes").forEach((fichier) => {
                     if (path.extname(fichier).toLowerCase() == (".js")) {
                         try {
@@ -882,9 +882,9 @@ zk.ev.on('group-participants.update', async (group) => {
                 
                 if((conf.DP).toLowerCase() === 'yes') {     
 
-                let cmsg =`      ʀᴀʜᴍᴀɴɪ-xᴍᴅ ᴄᴏɴɴᴇᴄᴛᴇᴅ
+                let cmsg =`      ➣➣➣➣➣➣➣➣➣➣➣➣
 ╭─────────────━┈⊷ 
-│🌏 RAHMAN CONNECTED
+│🌏 lezkush CONNECTED
 │💫 ᴘʀᴇғɪx: *[ ${prefixe} ]*
 │⭕ ᴍᴏᴅᴇ: *${md}*
 ╰─────────────━┈⊷⁠⁠⁠⁠`;
